75d420de2f14de89e337860c0c6b7044733d4a09
GGojae <woajrsla100@gmail.com>
2025-11-10 21:18:37 +0900

    fix(webconfig) : Enum Dto 들 Converter 등록 누락 건/ 등록 완료- chore(auth): dto -> response, request 로 변경

--- backend/src/main/java/com/gj/dev_note/config/WebConfig.java
+++ backend/src/main/java/com/gj/dev_note/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.gj.dev_note.config;
 
 import com.gj.dev_note.artifact.dto.ArtifactGroupTypeDto;
+import com.gj.dev_note.category.dto.CategoryScopeDto;
+import com.gj.dev_note.common.VisibilityDto;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,5 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(String.class, ArtifactGroupTypeDto.class, ArtifactGroupTypeDto::from);
+        registry.addConverter(String.class, CategoryScopeDto.class, CategoryScopeDto::from);
+        registry.addConverter(String.class, VisibilityDto.class, VisibilityDto::from);
     }
 }
