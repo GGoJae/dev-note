e5fecc9c9d4774b9578a456db36df8510cc16ce2
GGojae <woajrsla100@gmail.com>
2025-11-13 07:41:34 +0900

    refactor(backend:webconfig) : 웹설정에 converterFactory 등록

--- backend/src/main/java/com/gj/dev_note/config/WebConfig.java
+++ backend/src/main/java/com/gj/dev_note/config/WebConfig.java
@@ -1,18 +1,20 @@
 package com.gj.dev_note.config;
 
 import com.gj.dev_note.artifact.dto.ArtifactGroupTypeDto;
-import com.gj.dev_note.category.dto.CategoryScopeDto;
-import com.gj.dev_note.common.VisibilityDto;
+import com.gj.dev_note.common.enums.EnumDtoConverterFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final EnumDtoConverterFactory enumDtoConverterFactory;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(String.class, ArtifactGroupTypeDto.class, ArtifactGroupTypeDto::from);
-        registry.addConverter(String.class, CategoryScopeDto.class, CategoryScopeDto::from);
-        registry.addConverter(String.class, VisibilityDto.class, VisibilityDto::from);
+        registry.addConverterFactory(enumDtoConverterFactory);
     }
 }
